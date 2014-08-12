33bdc3e03ab8af08e71f12067d922070367f9c0c
viki53 <viki53000@gmail.com>
2014-08-13 00:30:33 +0200

    Test channel + events listeners

--- js/nick-v1.js
+++ js/nick-v1.js
@@ -1,15 +1,62 @@
 var irc = require("irc");
 
-var client = new irc.Client("irc.smoothirc.net", "Nick_tests", {
-    channels: ["#zestedesavoir"],
+
+var app = {};
+
+
+app.client = new irc.Client("irc.smoothirc.net", "Nick", {
+	channels: [
+		// "#zestedesavoir",
+		"#nick_tests",
+	]
 });
 
-client.addListener('message', function (from, to, message) {
-	console.dir(arguments);
+app.tabs = [];
+
+app.client.addListener("message", function (from, to, message) {
+	console.log("message : ", arguments);
 
 	var p = document.createElement("p");
-
 	p.innerText = from + " => " + to + " : " + message;
+	document.body.appendChild(p);
 
+	window.scrollTop = document.body.scrollHeight;
+});
+
+app.client.addListener("pm", function (from, message) {
+	console.log("pm : ", arguments);
+
+	var p = document.createElement("p");
+	p.innerText = from + " => " + to + " : " + message;
+	document.body.appendChild(p);
+
+	window.scrollTop = document.body.scrollHeight;
+});
+
+app.client.addListener("names", function (channel, nicks) {
+	console.log("names : ", arguments);
+});
+
+app.client.addListener("nick", function (oldnick, newnick, channels, message) {
+	console.log("nick : ", arguments);
+
+	var p = document.createElement("p");
+	p.innerText = oldnick + " will now be called " + newnick;
 	document.body.appendChild(p);
 });
+
+app.client.addListener("join", function (channel, nick, message) {
+	console.log("join : ", arguments);
+
+	var p = document.createElement("p");
+	p.innerText = nick + " joined " + channel;
+	document.body.appendChild(p);
+});
+
+app.client.addListener("error", function(message) {
+	console.log("error : ", message);
+});
+
+console.dir(irc.colors.codes);
+
+app.client.send("NICK", "Nick_tests_changed");
